c3b282166ecc4902b235522c035e885e5f4f48cb
Георгий <nazimov03@list.ru>
2026-03-14 01:17:19 +0800

    orders rename total to total_price

---
+++ migrations/versions/2026_03_10_2210_rename_order_total_column_to_total_price.py
@@ -0,0 +1,34 @@
+"""rename order total column to total_price
+
+Revision ID: b3007c62d51b
+Revises: 74dfd6ac44be
+Create Date: 2026-03-10 22:10:37.335924
+
+"""
+from typing import Sequence, Union
+
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision: str = 'b3007c62d51b'
+down_revision: Union[str, None] = '74dfd6ac44be'
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+
+
+def upgrade() -> None:
+    op.alter_column(
+        'orders',
+        'total',
+        new_column_name='total_price'
+    )
+
+
+def downgrade() -> None:
+    op.alter_column(
+        'orders',
+        'total_price',
+        new_column_name='total'
+    )
