e42c6d0d252d4bcebaf3f2eca249bc0154f6aaf0
Георгий <nazimov03@list.ru>
2026-03-29 03:25:16 +0800

    refactor: refactor old tests and add api tests

--- tests/services/test_user/test_service_user_crud.py
+++ tests/services/test_user/test_service_user_crud.py
@@ -4,14 +4,14 @@ from jose import jwt
 import pytest
 from sqlalchemy import select
 
-from app.core.exceptions import AuthenticationError, ConflictError
+from app.core.exceptions import AuthenticationError, ConflictError, NotFoundError
 from app.database.models.user import User
-from app.services.user import login_serv, create_user_serv
+from app.services.user import get_user_data_serv, login_serv, create_user_serv
 from tests.factories.users import new_user_data_factory, user_factory
 
 
 @pytest.mark.asyncio
-async def test_create_user_unique_email(db_session):
+async def test_create_user_unique_email_success(db_session):
     new_user = new_user_data_factory()
 
     await create_user_serv(new_user, db_session)
@@ -22,7 +22,7 @@ async def test_create_user_unique_email(db_session):
 
 
 @pytest.mark.asyncio
-async def test_create_user_same_email(db_session):
+async def test_create_user_same_email_error(db_session):
     new_user1 = new_user_data_factory(email="same_email@domain.com")
     new_user2 = new_user_data_factory(email="same_email@domain.com")
 
@@ -32,7 +32,7 @@ async def test_create_user_same_email(db_session):
 
 
 @pytest.mark.asyncio
-async def test_authenticate_user(db_session, test_settings):
+async def test_login_user_success(db_session, test_settings):
     new_user = user_factory()
     db_session.add(new_user)
     await db_session.flush()
@@ -41,7 +41,9 @@ async def test_authenticate_user(db_session, test_settings):
     login_result = await login_serv(auth_data, db_session)
 
     token_data = jwt.decode(
-        login_result.access_token, test_settings.SECRET_KEY, algorithms=[test_settings.ALGORITHM]
+        login_result.access_token,
+        test_settings.SECRET_KEY,
+        algorithms=[test_settings.ALGORITHM],
     )
 
     assert token_data["sub"] == str(new_user.id)
@@ -49,7 +51,7 @@ async def test_authenticate_user(db_session, test_settings):
 
 
 @pytest.mark.asyncio
-async def test_authenticate_user_wrong_email(db_session):
+async def test_login_user_wrong_email_error(db_session):
     new_user = user_factory()
     db_session.add(new_user)
     await db_session.flush()
@@ -60,7 +62,7 @@ async def test_authenticate_user_wrong_email(db_session):
 
 
 @pytest.mark.asyncio
-async def test_authenticate_user_wrong_password(db_session):
+async def test_login_user_wrong_password_error(db_session):
     new_user = user_factory()
     db_session.add(new_user)
     await db_session.flush()
@@ -68,3 +70,23 @@ async def test_authenticate_user_wrong_password(db_session):
 
     with pytest.raises(AuthenticationError):
         await login_serv(auth_data, db_session)
+
+
+@pytest.mark.asyncio
+async def test_get_user_success(db_session):
+    new_user = user_factory()
+    db_session.add(new_user)
+    await db_session.flush()
+
+    user_data = await get_user_data_serv(new_user, db_session)
+
+    assert user_data.email == new_user.email
+    assert user_data.role == new_user.role
+
+
+@pytest.mark.asyncio
+async def test_get_user_not_found(db_session):
+    new_user = user_factory()
+
+    with pytest.raises(NotFoundError):
+        await get_user_data_serv(new_user, db_session)
